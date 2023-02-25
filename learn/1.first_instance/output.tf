fa5e63058689c7b79291b7626c4c3a86bc314eb2
Arturo Lopez Salgado <lopezsalgado.a@gmail.com>
2023-02-25 01:01:36 -0600

    6. join

--- learn/1.first_instance/output.tf
+++ learn/1.first_instance/output.tf
@@ -9,3 +9,7 @@ output "instance_name" {
 output "zone" {
   value = "google_compute_instance.instance-test.zone"
 }
+
+output "instance_id" {
+  value = join(",", google_compute_instance.instance-test.*.id)
+}
